be00c48c0ec2d824ba8f582cb10e3566f61c1eea
Asrinne Nazari <asse_89@hotmail.com>
2025-03-13 17:49:13 +0100

    update navbar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,40 +1,10 @@
-import github from "/src/img/icons8-github-50.png";
-import instagram from "/src/img/icons8-instagram-logo-50.png";
-import linkedin from "/src/img/icons8-linkedin-50.png";
-
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-8 h-32">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <div className="mb-4 md:mb-0">
-          <p>© {new Date().getFullYear()} Asrinne Nazari</p>
-        </div>
-        <div className="flex space-x-4 mt-4 md:mt-0">
-          <a
-            href="www.linkedin.com/in/asrinne-nazari"
-            aria-label="Linked in"
-            className="hover:text-gray-400"
-          >
-            <img src={linkedin} alt="linkedin" />
-          </a>
-          <a
-            href="https://github.com/AsrinneNazari"
-            aria-label="Github"
-            className="hover:text-gray-400"
-          >
-            <img src={github} alt="github" />
-          </a>
-          <a
-            href="https://www.instagram.com/asrinne"
-            aria-label="Instagram"
-            className="hover:text-gray-400"
-          >
-            <img src={instagram} alt="instagram" />
-          </a>
-        </div>
+    <footer className="bg-gray-800 text-white py-2 h-10">
+      <div className="container mx-auto flex justify-center items-center h-full">
+        <p className="text-center">© {new Date().getFullYear()} Asrinne Nazari</p>
       </div>
     </footer>
   );
 };
-
 export default Footer;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,65 +1,45 @@
 import { NavLink } from "react-router-dom";
-import { Menu, X } from "lucide-react";
-import { useEffect, useState } from "react";
+import github from "/src/img/icons8-github-50.png";
+import instagram from "/src/img/icons8-instagram-logo-50.png";
+import linkedin from "/src/img/icons8-linkedin-50.png";
 
 interface INavLinkProps {
   click: () => void;
 }
 
-const NavLinks = ({ click }: INavLinkProps) => {
+const Nav = ({ click }: INavLinkProps) => {
   return (
-    <>
+    <div className="flex justify-end items-center w-full py-5">
       <NavLink to="/" className="relative text-lg" onClick={click}>
         Home
-      </NavLink>
-      <NavLink to="/about" className="relative text-lg" onClick={click}>
-        About 
       </NavLink>
       <NavLink to="/projects" className="relative text-lg" onClick={click}>
         Projects
       </NavLink>
-      <NavLink to="/contact" className="relative text-lg" onClick={click}>
-        Contact
-      </NavLink>
-    </>
-  );
-};
-
-const Nav = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleBar = () => {
-    setIsOpen(!isOpen);
-  };
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth >= 768) {
-        setIsOpen(false);
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  return (
-    <div className="flex justify-center items-center w-full py-5 border-b border-gray-300">
-      <nav className="md:flex md:justify-center flex-1">
-        <div className="hidden md:flex gap-40">
-          <NavLinks click={toggleBar} />
-        </div>
-      </nav>
-      {isOpen && (
-        <div className="flex flex-col mt-4 w-full justify-center items-center md:hidden px-8 gap-12">
-          <NavLinks click={toggleBar} />
-        </div>
-      )}
-      <div className="absolute right-4 top-4 md:hidden">
-        <button onClick={toggleBar}>{isOpen ? <X /> : <Menu />}</button>
-      </div>
+      <a
+        href="https://linkedin.com/in/asrinne-nazari"
+        aria-label="Linked in"
+        className="w-8 h-8 hover:opacity-70 transition-opacity"
+      >
+        <img src={linkedin} alt="linkedin" />
+      </a>
+      <a
+        href="https://github.com/AsrinneNazari"
+        aria-label="Github"
+        className="w-8 h-8 hover:opacity-70 transition-opacity"
+      >
+        <img src={github} alt="github" />
+      </a>
+      <a
+        href="https://www.instagram.com/asrinne"
+        aria-label="Instagram"
+        className="w-8 h-8 hover:opacity-70 transition-opacity"
+      >
+        <img src={instagram} alt="instagram" />
+      </a>
+      <button className="bg-purple-400 text-white py-4 px-6 rounded hover:bg-purple-600 transition-colors duration-300">
+        Mitt CV
+      </button>
     </div>
   );
 };
