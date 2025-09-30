0ab48ce58c8f9f3281ee01d68ff3bc1ea5711b51
Asrinne Nazari <asse_89@hotmail.com>
2025-09-30 16:47:34 +0200

    carefully remove tailwind and do mobile first, new styling and shit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,16 +2,12 @@ import picToPortfolio from "./../img/picToPortfolio.jpg";
 
 export const Home = () => {
   return (
-    <div className="h-screen flex flex-col items-center justify-center p-4">
-      <div className="flex flex-col md:flex-row items-center gap-8 md:gap-16">
-        <div className="max-w-md text-center md:text-left">
-          <h1 className="text-4xl md:text-6xl font-serif mb-4">
-            Asrinne Nazari
-          </h1>
-          <h1 className="text-2xl md:text-4xl font-serif">
-            Front-end developer
-          </h1>
-          <p className="text-sm md:text-base mt-6">
+    <div className="mainContainer">
+      <div className="mainContentContainer">
+        <div className="bioContainer">
+          <h1 className="mainHeader">Asrinne Nazari</h1>
+          <h1 className="subHeader">Front-end developer</h1>
+          <p className="mainBio">
             Hi! I’m Asrinne, a frontend developer who transitioned from a career
             as an X-ray technician. With a strong foundation in HTML, CSS, and
             JavaScript, I create responsive, user-friendly websites that deliver
@@ -23,23 +19,20 @@ export const Home = () => {
           </p>
         </div>
 
-        <div className="mt-6 md:mt-0">
-          <img
-            src={picToPortfolio}
-            alt="picToPortfolio"
-            className="w-64 md:w-96"
-          />
+        <div className="mainPic">
+          <img src={picToPortfolio} alt="picToPortfolio" />
         </div>
       </div>
       <div>
-        <div className="text-4xl md:text-4xl font-serif">Tech Stack</div>
-        <div className="flex flex-col md:flex-row">
+        <div className="techInfo">Tech Stack</div>
+        <div className="techContentContainer">
+        <div className="techContainer">
           <img src="/src/img/html5-icon.png" alt="html5" />
           <img src="/src/img/CSS-icon.png" alt="css" />
           <img src="/src/img/JS-icon.png" alt="javascript" />
           <img src="/src/img/" alt="react" />
           <img src="/src/img/Vue-icon.png" alt="vue" />
-
+        </div>
         </div>
       </div>
     </div>
