c0893f9726f568aa386e94416b69496077a54cd8
Asrinne Nazari <asse_89@hotmail.com>
2025-11-11 12:34:12 +0100

    some styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,19 +3,19 @@ import linkedin from "/src/img/icons8-linkedin-50.png";
 
 const Footer = () => {
   return (
-    <footer className="bg-white text-white py-2 min-h-10">
-      <div className="flex justify-center items-center h-full gap-x-6">
+    <footer className="footer">
+      <div className="footerInner">
         <a
           href="https://linkedin.com/in/asrinne-nazari"
           aria-label="Linked in"
-          className="w-8 h-8 hover:opacity-70 transition-opacity"
+          className="footerIcon"
         >
           <img src={linkedin} alt="linkedin" />
         </a>
         <a
           href="https://github.com/AsrinneNazari"
           aria-label="Github"
-          className="w-8 h-8 hover:opacity-70 transition-opacity"
+          className="footerIcon"
         >
           <img src={github} alt="github" />
         </a>
