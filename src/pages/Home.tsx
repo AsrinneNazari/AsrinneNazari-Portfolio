2a9b5e9e3d699e8264962669e510150ac9bb0027
Asrinne Nazari <asse_89@hotmail.com>
2026-01-15 19:22:20 +0100

    fix deploy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import ContactForm  from "../components/ContactForm.tsx";
+import ContactForm from '../components/ContactForm'
 import { ContactInfo } from "../components/ContactInfo";
 import TechStack from "../components/TechStack";
 import picToPortfolio from "./../img/picToPortfolio.jpg";
