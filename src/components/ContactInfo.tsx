4b92f4afb3e66c4919dfb7c7c3cf921f542e1f9b
Asrinne Nazari <asse_89@hotmail.com>
2026-03-19 11:51:41 +0100

    update contact form styling

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -1,6 +1,19 @@
-export const ContactInfo = () =>{
+import { MapPin } from "lucide-react";
 
-    return <>
-    <div>If you have questions or just want to chat, contact me! :)</div>
-    </>
-}
+export const ContactInfo = () => {
+  return (
+    <div className="contactInfoText">
+      <h3 className="contactTitle">Let’s connect ✨</h3>
+
+      <p className="contactDescription">
+        Have a question, an idea, or just want to say hi?  
+        I’m always open to talking about new opportunities, projects, or anything frontend-related.
+      </p>
+
+      <div className="contactLocation">
+        <MapPin size={18} />
+        <span>Stockholm, Sweden</span>
+      </div>
+    </div>
+  );
+};
